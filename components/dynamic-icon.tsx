e1a8a58448835e2e3a2300d7f77d23feebe3a118
Isabella Leyton Carmona <leytoncarmonaisabella@gmail.com>
2026-08-11 21:24:23 -0500

    add icon

--- components/dynamic-icon.tsx
+++ components/dynamic-icon.tsx
@@ -1,4 +1,5 @@
-import { DynamicIcon as LucideDynamicIcon, type LucideProps } from "lucide-react/dynamic"
+import { DynamicIcon as LucideDynamicIcon } from "lucide-react/dynamic"
+import type { LucideProps } from "lucide-react"
 
 export function DynamicIcon({ name, ...props }: { name: string } & LucideProps) {
   return <LucideDynamicIcon name={name as any} {...props} />
